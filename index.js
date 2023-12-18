2db2ab7091f19b923779c46f74e62fc97c14d391
Rich90U <147627031+Rich90U@users.noreply.github.com>
2023-12-18 16:08:51 +0000

    used jQuery to add an if statement and add a class to time block. Added local storage for events

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ $(document).ready(function () {
       var hourCol = $("<div>").addClass("col-1 hour").text(formatHour(hour));
       var textAreaCol = $("<textarea>").addClass("col-10 description").attr("data-hour", hour);
       var saveBtnCol = $("<button>").addClass("col-1 saveBtn").html('<i class="fas fa-save"></i>');
+
+      if (hour < currentHour) {
+        timeBlock.addClass("past");
+      } else if (hour === currentHour) {
+        timeBlock.addClass("present");
+      } else {
+        timeBlock.addClass("future");
+      }
+
+      
+      var storedEvent = localStorage.getItem("event_" + hour);
+      if (storedEvent) {
+        textAreaCol.val(storedEvent);
+      }
+
+      
